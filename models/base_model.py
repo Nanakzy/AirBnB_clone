af5f165d21ee15b8a431216b7caa806ac2217257
Nana <nanakzy@gmail.com>
2024-02-11 14:50:48 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,21 @@ class BaseModel:
             id (str): A unique identifier generated using uuid.
             created_at current date & time when instance is created.
             updated_at current date & time when instance is created.
+            *args: Unused positional arguments.
+            **kwargs: Arbitrary keyword arguments representing instance
+            Keys are attri names, values are corresponding attri values.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
